048a6b08c902755d7beda80547b76b11b4efac1f
harlandski <harlandski@gmail.com>
2023-08-10 15:27:43 +0600

    Added comments to non-working code

--- assets/scripts/card-translator.js
+++ assets/scripts/card-translator.js
@@ -1,5 +1,6 @@
 const mtg = require('mtgsdk')
 
+//This part works, apart from the form
 mtg.card.where({name: 'Гоблин-Цепехлест', language: 'russian'})
 .then(results => {
     for (card of results) {
@@ -9,11 +10,14 @@ mtg.card.where({name: 'Гоблин-Цепехлест', language: 'russian'})
 
 const russianCardName = document.getElementById("russian-cardname");
 const submit = document.getElementById("russian-button");
+// This is not used yet, but in the end will be used to output to a div with this id
 const englishCardName = document.getElementById("english-cardname");
 
 const translateCardName = () => {
     const translationQuery = russianCardName.value;
+    // This console log works, showing that the function is being called
     console.log(translationQuery);
+    // None of this seems to work:
     mtg.card.where({ name: translationQuery, language: 'russian' })
         .then(results => {
             for (card of results) {
