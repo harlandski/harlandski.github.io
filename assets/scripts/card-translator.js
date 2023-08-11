ae6725678754806d70bb7fffdc9741925eb5c973
harlandski <harlandski@gmail.com>
2023-08-11 11:17:28 +0600

    Added error catching

--- assets/scripts/card-translator.js
+++ assets/scripts/card-translator.js
@@ -13,17 +13,21 @@ const submit = document.getElementById("russian-button");
 // This is not used yet, but in the end will be used to output to a div with this id
 const englishCardName = document.getElementById("english-cardname");
 
-const translateCardName = () => {
+const translateCardName = async () => {
     const translationQuery = russianCardName.value;
     // This console log works, showing that the function is being called
     console.log(translationQuery);
     // None of this seems to work:
-    mtg.card.where({ name: translationQuery, language: 'russian' })
+    try {await mtg.card.where({ name: translationQuery, language: 'russian' })
         .then(results => {
             for (card of results) {
                 console.log(card.name);
             }
         })
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
 
 submit.addEventListener('click', translateCardName);
